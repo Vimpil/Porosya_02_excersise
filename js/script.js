9a8a6ebbc3557dacc1927ccfd7766b04da7025ed
Vimpil <vimpil@ya.ru>
2016-08-23 16:28:24 +0300

    Added flexlider navigation sprites; fancybox inside flixslider gallary;

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
 $(document).ready(function() {
-
 	// BEGIN FLEXSLIDER
 
 	$('.flexslider').flexslider({
-        animation: "fade", // slide or fade
-        controlsContainer: ".flex-container" // the container that holds the flexslider
-    });
+		animation: "fade", // slide or fade
+		controlsContainer: ".flex-container" // the container that holds the flexslider
+	});
+	$('.flexslider').flexslider('pause');
 
-    $("div.catal_arr_right").click(function() {
-    	console.log("right_down");
-        $(".gal_boxes " + " a.flex-next").click();
-    });
+	$("div.catal_arr_right").click(function() {
+		console.log("right_down");
+		$(".gal_boxes " + " a.flex-next").click();
+	});
 
-    $("div.catal_arr_left").click(function() {
-        $(".gal_boxes " + " a.flex-prev").click();
-    });
+	$("div.catal_arr_left").click(function() {
+		$(".gal_boxes " + " a.flex-prev").click();
+	});
 
 	// END FLEXSLIDER
 
 	// GALLARY FLEXSLIDER
 	var li_flexl_count
-	$.each($(".srub_main_gal .flex-control-nav li"),function() {
-		$(this).append('<span>'+($(this).index()+1)+'</span>');
+	$.each($(".srub_main_gal .flex-control-nav li"), function() {
+		$(this).append('<span>' + ($(this).index() + 1) + '</span>');
 	});
-	li_flexl_count=0;
-	$.each($(".srub_elements_gal .flex-control-nav li"),function() {
-		$(this).append('<span>'+($(this).index()+1)+'</span>');
+	li_flexl_count = 0;
+	$.each($(".srub_elements_gal .flex-control-nav li"), function() {
+		$(this).append('<span>' + ($(this).index() + 1) + '</span>');
 	});
 
 	// END GALLARY FLEXSLIDER
@@ -49,21 +49,39 @@ $(document).ready(function() {
 
 	//END Customer submenu
 
-	var fancybox_options_array ={
-				"wrapCSS": "call",
-				"padding": 0,
-				helpers: {
-					overlay: {
-						locked: false,
-						opacity: 0.1,
-						css: {
-							'background': 'rgba(90, 92, 94, 0.8)'
-						}
-					}
+	var fancybox_present_options_array = {
+		"wrapCSS": "call",
+		"padding": 0,
+		helpers: {
+			overlay: {
+				locked: false,
+				opacity: 0.1,
+				css: {
+					'background': 'rgba(90, 92, 94, 0.8)'
 				}
 			}
+		}
+	}
+
+	var fancybox_elements_gal_options_array ={
+		"wrapCSS": "call",
+		"padding": 0,
+		helpers: {
+			overlay: {
+				locked: false,
+				opacity: 0.1,
+				css: {
+					'background': 'rgba(90, 92, 94, 0.8)'
+				}
+			}
+		}
+	};
 
 	$("a.present_window").click(function(event) {
-			$("a.present_window").fancybox(fancybox_options_array)
-		})
+		$("a.present_window").fancybox(fancybox_present_options_array)
+	})
+	$("a.fancyboxGal").click(function(event) {
+		/* Act on the event */
+		$("a.fancyboxGal").fancybox(fancybox_elements_gal_options_array);
+	});
 })
